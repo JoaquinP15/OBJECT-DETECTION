cc6bd44d8eeb6776d53456be6aff8cdb9dca9663
JoaquinP15 <94157179+JoaquinP15@users.noreply.github.com>
2022-08-11 15:20:02 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function bed_room(){
+    window.location = "bed_room.html";
+}
+
+function garden(){
+    window.location = "garden.html";
+}
+
+function kitchen(){
+    window.location = "kitchen.html";
+}
